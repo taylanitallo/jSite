9bbdb8c905afbb5a0f049715eb579013af6a76a0
Taylan Itallo <taylan.itallo@gmail.com>
2026-04-03 19:35:49 -0300

    fix: corrigir rota /jprocesso com iFrame

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./app/App.tsx";
+import JProcesso from "./app/pages/JProcesso.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import { BrowserRouter, Routes, Route } from "react-router-dom";
-  import App from "./app/App.tsx";
-  import JProcesso from "./app/pages/JProcesso.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/jprocesso/*" element={<JProcesso />} />
-      </Routes>
-    </BrowserRouter>
-  );
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/jprocesso/*" element={<JProcesso />} />
+    </Routes>
+  </BrowserRouter>
+);
